a7f873b7d3f3424f33e8b9d7a58187f91048aa37
Adil Ferryat <aferryat@e2r9p15.1337.ma>
2026-04-24 15:45:20 +0100

    remove (saving data (username password in localhost removed))

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -6,22 +6,9 @@ import "../app.css";
 
 export default function DashboardPage() {
 
-  const [user, setUser] = useState<string | null>(null);
-  const [password, setPassword] = useState<string | null>(null);
-
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    setUser(storedUser);
-
-    if (storedUser) {
-      setPassword(localStorage.getItem(storedUser));
-    }
-  }, []);
-
-
-  if (!user || !password) {
-    return <Forbidden />;
-  }
+  // if (!user || !password) {
+  //   return <Forbidden />;
+  // }
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#cde0ff] via-[#e0f0ff] to-[#f8e1f0] flex items-center justify-center p-6">
